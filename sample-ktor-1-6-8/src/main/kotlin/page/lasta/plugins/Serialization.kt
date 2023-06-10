ae603270986e2fc6175f0314761bfde448c34905
Atsushi Koshikizawa <lastarrow21@gmail.com>
2023-06-10 15:54:23 +0900

    [1.6.8] fix import errors

--- sample-ktor-1-6-8/src/main/kotlin/page/lasta/plugins/Serialization.kt
+++ sample-ktor-1-6-8/src/main/kotlin/page/lasta/plugins/Serialization.kt
@@ -1,12 +1,10 @@
 package page.lasta.plugins
 
-import io.ktor.serialization.*
-import io.ktor.features.*
-import io.ktor.server.response.*
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
