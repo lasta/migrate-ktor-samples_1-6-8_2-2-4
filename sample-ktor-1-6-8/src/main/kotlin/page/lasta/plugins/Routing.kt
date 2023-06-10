bfea58f691a05f95355c0a0b4cb8881727687324
Atsushi Koshikizawa <lastarrow21@gmail.com>
2023-06-10 18:46:38 +0900

    add sample - receiveOrNull to receiveNullable

--- sample-ktor-1-6-8/src/main/kotlin/page/lasta/plugins/Routing.kt
+++ sample-ktor-1-6-8/src/main/kotlin/page/lasta/plugins/Routing.kt
@@ -1,11 +1,12 @@
 package page.lasta.plugins
 
-import io.ktor.routing.*
+import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.locations.*
-import io.ktor.application.*
-import io.ktor.response.*
+import io.ktor.locations.post
 import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
 fun Application.configureRouting() {
     install(Locations) {
@@ -24,11 +25,17 @@ fun Application.configureRouting() {
         get<Type.List> {
             call.respondText("Inside $it")
         }
+
+        post<ArticleLocation> {
+            val article = call.receiveOrNull<Article>()
+            TODO("do something")
+        }
     }
 }
 
 @Location("/location/{name}")
 class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+
 @Location("/type/{name}")
 data class Type(val name: String) {
     @Location("/edit")
@@ -37,3 +44,8 @@ data class Type(val name: String) {
     @Location("/list/{page}")
     data class List(val type: Type, val page: Int)
 }
+
+@Location("/articles")
+data class ArticleLocation(val id: Int)
+
+data class Article(val title: String, val author: String, val body: String)
--- sample-ktor-2-2-4/src/main/kotlin/page/lasta/plugins/Routing.kt
+++ sample-ktor-2-2-4/src/main/kotlin/page/lasta/plugins/Routing.kt
@@ -1,12 +1,14 @@
 package page.lasta.plugins
 
-import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.resources.*
 import io.ktor.resources.*
 import io.ktor.server.resources.Resources
 import kotlinx.serialization.Serializable
 import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.resources.post
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     install(Resources)
@@ -18,9 +20,16 @@ fun Application.configureRouting() {
             // Get all articles ...
             call.respond("List of articles sorted starting from ${article.sort}")
         }
+
+        post<Articles> {
+            val article = runCatching { call.receiveNullable<ArticleBody>() }.getOrNull()
+            TODO("do something")
+        }
     }
 }
 
 @Serializable
 @Resource("/articles")
 class Articles(val sort: String? = "new")
+
+class ArticleBody(val title: String, val author: String, val body: String)
