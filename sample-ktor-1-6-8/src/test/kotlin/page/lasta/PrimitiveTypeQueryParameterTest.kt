74d7c358e090bc7bcc4d21170a42a86ea7fc230a
Atsushi Koshikizawa <lastarrow21@gmail.com>
2023-06-18 17:48:23 +0900

    [1.6.8] add test cases for optional parameters

--- sample-ktor-1-6-8/src/test/kotlin/page/lasta/PrimitiveTypeQueryParameterTest.kt
+++ sample-ktor-1-6-8/src/test/kotlin/page/lasta/PrimitiveTypeQueryParameterTest.kt
@@ -7,57 +7,132 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.testing.*
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.provider.ArgumentsSources
 import page.lasta.plugins.configureDataConversion
+import java.util.stream.Stream
 
 @OptIn(KtorExperimentalLocationsAPI::class)
 class PrimitiveTypeQueryParameterTest {
 
-    @Location(PATH)
+    @ArgumentsSources(
+        ArgumentsSource(RequiredParameterTestCase::class),
+        ArgumentsSource(OptionalParameterTestCases::class),
+    )
+    @ParameterizedTest(name = "{0}, then returns {2}")
+    fun test(
+        @Suppress("UNUSED_PARAMETER") name: String,
+        path: String,
+        queryString: String,
+        expected: HttpStatusCode,
+    ) {
+        withHandleRequest(path = path, queryString = queryString) {
+            assertEquals(expected, response.status())
+        }
+    }
+
+    private class RequiredParameterTestCase : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            arguments(
+                "When the required parameter is missing",
+                REQUIRED_PARAMETER_PATH,
+                "",
+                HttpStatusCode.NotFound,
+            ),
+            arguments(
+                "When only the key is provided",
+                REQUIRED_PARAMETER_PATH,
+                "?intValue",
+                HttpStatusCode.BadRequest,
+            ),
+            arguments(
+                "When there is no right-hand side",
+                REQUIRED_PARAMETER_PATH,
+                "?intValue=",
+                HttpStatusCode.BadRequest,
+            ),
+            arguments(
+                "When the required parameter is provided",
+                REQUIRED_PARAMETER_PATH,
+                "?intValue=42",
+                HttpStatusCode.OK,
+            ),
+        )
+    }
+
+    private class OptionalParameterTestCases : ArgumentsProvider {
+
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            arguments(
+                "When the required parameter is missing",
+                OPTIONAL_PARAMETER_PATH,
+                "",
+                HttpStatusCode.OK,
+            ),
+            arguments(
+                "When only the key is provided",
+                OPTIONAL_PARAMETER_PATH,
+                "?intValue",
+                HttpStatusCode.BadRequest,
+            ),
+            arguments(
+                "When there is no right-hand side",
+                OPTIONAL_PARAMETER_PATH,
+                "?intValue=",
+                HttpStatusCode.BadRequest,
+            ),
+            arguments(
+                "When the required parameter is provided",
+                OPTIONAL_PARAMETER_PATH,
+                "?intValue=42",
+                HttpStatusCode.OK,
+            ),
+        )
+    }
+
+    @Location(REQUIRED_PARAMETER_PATH)
     @Suppress("unused")
-    class WithoutDefaultValuesLocation(
+    class RequiredParameterLocation(
         val intValue: Int?,
     )
 
-    @Nested
-    inner class RequiredParameterTest {
+    @Location(OPTIONAL_PARAMETER_PATH)
+    @Suppress("unused")
+    class OptionalParameterLocation(
+        val intValue: Int? = 42,
+    )
 
-        private fun withHandleRequest(
-            queryString: String,
-            block: TestApplicationCall.() -> Unit
-        ) {
-            withTestApplication(
-                {
-                    configureDataConversion()
-                    install(Locations)
-                    routing {
-                        get<WithoutDefaultValuesLocation> {
-                            call.respond(HttpStatusCode.OK)
-                        }
+    private fun withHandleRequest(
+        path: String,
+        queryString: String,
+        block: TestApplicationCall.() -> Unit
+    ) {
+        withTestApplication(
+            {
+                configureDataConversion()
+                install(Locations)
+                routing {
+                    get<RequiredParameterLocation> {
+                        call.respond(HttpStatusCode.OK)
+                    }
+                    get<OptionalParameterLocation> {
+                        call.respond(HttpStatusCode.OK)
                     }
                 }
-            ) {
-                handleRequest(HttpMethod.Get, "$PATH$queryString").run(block)
             }
-        }
-
-        @Test
-        fun `when a required parameter is missing then returns NotFound`() {
-            withHandleRequest("") {
-                assertEquals(HttpStatusCode.NotFound, response.status())
-            }
-        }
-
-        @Test
-        fun `when a required parameter is provided then returns OK`() {
-            withHandleRequest("?intValue=42") {
-                assertEquals(HttpStatusCode.OK, response.status())
-            }
+        ) {
+            handleRequest(HttpMethod.Get, "$path$queryString").run(block)
         }
     }
 
     companion object {
-        private const val PATH = "/test"
+        private const val REQUIRED_PARAMETER_PATH = "/test/required-parameter"
+        private const val OPTIONAL_PARAMETER_PATH = "/test/optional-parameter"
     }
 }
