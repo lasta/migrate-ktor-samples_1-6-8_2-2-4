8080c8661932cb4b49fe63910056c574d7b214aa
Atsushi Koshikizawa <lastarrow21@gmail.com>
2023-06-18 16:08:23 +0900

    [2.2.4] install JUnit 5

--- sample-ktor-2-2-4/build.gradle.kts
+++ sample-ktor-2-2-4/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.kotlin.dsl.exclude
+import org.gradle.kotlin.dsl.testImplementation
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -31,4 +34,11 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3") {
+        exclude(group = "junit", module = "junit") // junit5を使うため
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
